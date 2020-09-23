553dd5c5fbaaac7afb2f6c3c6b01073d1b0f38a9
Jani Tiainen <jani@tiainen.cc>
2020-09-23 18:50:38 +0300

    Added arguments for specifying starting location

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import argparse
 import pygame
 from game import Game
 
+parser = argparse.ArgumentParser(description="PythonixCoders PyWeek30")
+parser.add_argument("--map", dest="map_name", help="Map name to load")
+parser.add_argument("--spawn", dest="spawn_name", help="Spawn to start from")
+
 if __name__ == "__main__":
+    args = parser.parse_args()
+    args_dict = {k: v for k, v in vars(args).items() if v}
     pygame.init()
-    game = Game()
+    game = Game(**args_dict)
     game.run()
